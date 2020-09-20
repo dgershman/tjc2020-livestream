458433b16999e4fc22aa081919c4d62044ee8cdc
Danny Gershman <danny.gershman@gmail.com>
2020-09-19 22:44:55 -0400

    working script for stop/start medialive channel

--- medialive-channel-change.sh
+++ medialive-channel-change.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-if [ "$1" == "start" ] | [ "$1" == "stop" ]; then
-  aws medialive $1-channel --channel-id $2 --output table
+if [ "${1}" == "start" ] || [ "${1}" == "stop" ]; then
+  echo "${1} triggered on streaming channel ${2}."
+  aws medialive ${1}-channel --channel-id=${2} --output table
+  echo "Clearing CDN cache for distribution ${3}"
+  aws cloudfront create-invalidation --distribution-id=${3} --paths "/"
 fi
